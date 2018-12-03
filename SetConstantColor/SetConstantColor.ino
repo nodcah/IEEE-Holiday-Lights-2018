a2208af82d0de5ab9a452ec551fb3daa8fc07aed
nodcah <nodcah@gmail.com>
2018-12-03 11:43:39 -0500

    Finishes SetConstantColor without testing

--- SetConstantColor/SetConstantColor.ino
+++ SetConstantColor/SetConstantColor.ino
@@ -1,7 +1,48 @@
-void setup() {
+/*  SetConstantColor by Nodcah
+    Sets all or part of an LED strip to a single, constant color.
+    Uses library NeoPixelBus by Makuna for LED control.
+*/
 
+#include <NeoPixelBus.h>
+
+//====================================//
+//======== IMPORTANT VARIABLES =======//
+//====================================//
+/*  Example (o represents the LED is turned on):
+    ======oooooooooooooooooooooooo=====================
+          ^                       ^                    ^
+     startPixel               endPixel             PixelCount
+*/
+const int PixelCount = 150;  // Number of pixels on a strip (default is 150)
+const int PixelPin = 2;      // Pin that the LEDS are connected to on Arduino
+const int startPixel = 0;    // Pixel number on which to start control (inclusive)
+const int endPixel = 150;    // Pixel number on which to end control (exclusive)
+
+RgbColor color = RgbColor(100, 0, 0); // (r,g,b), Color to set strip to
+//HslColor color = HslColor(0.0f,1.0f,.196f);  // (hue,saturation,value), Color to set strip to
+
+
+//=============================//
+//======== Control Code =======//
+//=============================//
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
+void setup() {
+  Serial.begin(115200);
+
+  Serial.print("Initializing strip with color ");
+  Serial.println(color);
+  Serial.flush();  // Wait for transmission to finish
+
+  strip.Begin();
+
+  // Setting to  a color
+  for (int pixel = startPixel; pixel < endPixel; pixel++) {
+    else strip.SetPixelColor(pixel, color);
+  }
+  strip.Show();
 }
 
 void loop() {
-
+  // Do nothing
+  delay(1000);
 }
